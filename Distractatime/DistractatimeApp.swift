6553eb24302d5be2f6354eca201f071910aabdec
evan-gan <139721374+evan-gan@users.noreply.github.com>
2025-03-01 14:29:57 -0600

    Initial Commit

---
+++ Distractatime/DistractatimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistractatimeApp.swift
+//  Distractatime
+//
+//  Created by Evan Gan on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DistractatimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
